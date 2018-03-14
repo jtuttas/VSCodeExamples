7255f6793c091f9cefe50af3c87f32264b3e6b1e
jtuttas <jtuttas@gmx.net>
2018-03-14 10:27:16 +0100

    Nun mit Java Build Task

--- Java/Main1.java
+++ Java/Main1.java
@@ -1,3 +1,7 @@
+import java.net.ConnectException;
+import java.sql.DriverManager;
+import java.sql.Connection;
+
 public class Main1 {
     String name;
 
@@ -13,15 +17,26 @@ public class Main1 {
 
     /**
      * @param name the name to set
-     */
+     */ 
     public void setName(String name) {
         this.name = name;
     }
 
     public static void main(String[] args) {
-        System.out.println("Hallo Welt");
+        System.out.println("Hallo Welt!");
         Main1 m = new Main1();
         m.setName("test");
+
+        Connection myConnection;
+       // DriverManager.registerDriver(new com.mysql.jdbc.Driver ());
+       //Class.forName("com.mysql.jdbc.Driver");
+        try {
+            myConnection = (Connection)DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","");
+            System.out.println("My Connection is:"+myConnection);
+            myConnection.close();
+        } catch (Exception e) {
+            System.out.println("Exceptoin:"+e);
+        }
         
     }
 }
