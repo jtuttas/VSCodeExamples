305e65baa4992619d2551a78ed7baefcd7649780
Joerg Tuttas <jtuttas@gmx.net>
2018-07-25 16:30:06 +0200

    geht

--- JavaMaven/src/main/java/mmbbs/App.java
+++ JavaMaven/src/main/java/mmbbs/App.java
@@ -9,5 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Person p = new Person();
+        p.name="Hallo";
+        System.out.println(p.name);
+        
     }
 }
---
+++ JavaMaven/src/main/java/mmbbs/Person.java
@@ -0,0 +1,5 @@
+package mmbbs;
+
+public class Person{
+    public String name;
+}
